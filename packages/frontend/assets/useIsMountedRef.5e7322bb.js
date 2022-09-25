2d1007e327247fb16358721ca8109b875c679db5
Hideyoshi Moriya <hide@piyo.jp>
2022-09-25 20:30:55 +0900

    adjust UI

---
+++ packages/frontend/assets/useIsMountedRef.5e7322bb.js
@@ -0,0 +1,1 @@
+import{r}from"./index.8e2a867c.js";function s(){const e=r.exports.useRef(!1);return r.exports.useEffect(()=>(e.current=!0,()=>{e.current=!1}),[]),e}export{s as u};
--- packages/frontend/assets/useIsMountedRef.f86cd63f.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index.2bd8f465.js";function s(){const e=r.exports.useRef(!1);return r.exports.useEffect(()=>(e.current=!0,()=>{e.current=!1}),[]),e}export{s as u};
